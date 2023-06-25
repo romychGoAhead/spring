95e0f8413bcc109a1c95fdd8d5444e57094e6d03
eremi <ereminr83@mail.ru>
2023-06-25 20:27:24 +0700

    second commit

--- src/main/java/com/example/spring/service/BasketService.java
+++ src/main/java/com/example/spring/service/BasketService.java
@@ -1,14 +1,23 @@
 package com.example.spring.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Component
-
+@SessionScope
 public class BasketService {
     private final List<Long> ids = new ArrayList<>();
 
+    public void add(List<Long> ids) {
+        this.ids.addAll(ids);     // добавляем в корзину
+    }
+
+    public List<Long> getAll() {
+        return Collections.unmodifiableList(ids);                      // возвращает обернутый список чтобы его нельзя было изменить извне
+    }
 
 }
