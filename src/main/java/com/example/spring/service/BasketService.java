7ff72593a9361664d07587641823c1076fc4b38e
eremi <ereminr83@mail.ru>
2023-06-25 19:07:08 +0700

    first commit

---
+++ src/main/java/com/example/spring/service/BasketService.java
@@ -0,0 +1,14 @@
+package com.example.spring.service;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+
+public class BasketService {
+    private final List<Long> ids = new ArrayList<>();
+
+
+}
